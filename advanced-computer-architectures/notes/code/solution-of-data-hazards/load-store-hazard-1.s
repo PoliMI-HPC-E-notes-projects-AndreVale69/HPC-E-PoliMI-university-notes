63d1ecf41f39835d7df14d29c4592e7e30f8b07f
AndreVale69 <sforzini31@gmail.com>
2024-03-21 23:30:46 +0100

    [adv-computer-arch] Finished The Solution of Data Hazards

---
+++ advanced-computer-architectures/notes/code/solution-of-data-hazards/load-store-hazard-1.s
@@ -0,0 +1,2 @@
+lw $s0, VECTA($t1)  # $s0 <- M[VECTA + $t1]
+sw $s0, VECTB($t1)  # M[VECTA + $t1] <- $s0
---
+++ advanced-computer-architectures/notes/code/solution-of-data-hazards/load-use-hazard-1.s
@@ -0,0 +1,2 @@
+lw  $s0, 4($t1)     # $s0 <- M[4 + $t1]
+add $s5, $s0, $s1   # 1 operand $s0 depends from lw

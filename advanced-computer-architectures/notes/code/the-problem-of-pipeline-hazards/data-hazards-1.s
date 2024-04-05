f7cfa077034c355862acde62759684cb33d698a9
AndreVale69 <sforzini31@gmail.com>
2024-04-05 18:33:11 +0200

    [adv-computer-arch] Fixed indent errors

--- advanced-computer-architectures/notes/code/the-problem-of-pipeline-hazards/data-hazards-1.s
+++ advanced-computer-architectures/notes/code/the-problem-of-pipeline-hazards/data-hazards-1.s
@@ -1,4 +1,4 @@
-sub  $s2, $1, $3    # reg. $2 written by sub
+sub  $2, $1, $3     # reg. $2 written by sub
 and  $12, $2, $5    # 1 operand ($2) depends on sub
 or   $13, $6, $2    # 2 operand ($2) depends on sub
 add  $14, $2, $2    # 1 ($2) and 2 ($2) op.s depend on sub
